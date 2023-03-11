65bec10be64458bbf252bea547f783b859e6ac71
netoar <ntoar@hotmail.com>
2023-03-11 17:54:43 +0100

    makefile and first decorator

--- backend/src/api.py
+++ backend/src/api.py
@@ -11,36 +11,45 @@ app = Flask(__name__)
 setup_db(app)
 CORS(app)
 
-'''
-@TODO uncomment the following line to initialize the datbase
+"""
+@TODO uncomment the following line to initialize the database
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 !! Running this funciton will add one
-'''
-# db_drop_and_create_all()
+"""
+
+db_drop_and_create_all()
 
 # ROUTES
-'''
+"""
 @TODO implement endpoint
     GET /drinks
         it should be a public endpoint
         it should contain only the drink.short() data representation
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
-'''
+"""
 
 
-'''
+@app.route("/drinks")
+def headers():
+    auth_header = request.headers["Authorization"]
+    header_parts = auth_header.split(" ")[1]
+    print(header_parts)
+    return "not implemented"
+
+
+"""
 @TODO implement endpoint
     GET /drinks-detail
         it should require the 'get:drinks-detail' permission
         it should contain the drink.long() data representation
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
-'''
+"""
 
 
-'''
+"""
 @TODO implement endpoint
     POST /drinks
         it should create a new row in the drinks table
@@ -48,10 +57,10 @@ CORS(app)
         it should contain the drink.long() data representation
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
-'''
+"""
 
 
-'''
+"""
 @TODO implement endpoint
     PATCH /drinks/<id>
         where <id> is the existing model id
@@ -61,10 +70,10 @@ CORS(app)
         it should contain the drink.long() data representation
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
         or appropriate status code indicating reason for failure
-'''
+"""
 
 
-'''
+"""
 @TODO implement endpoint
     DELETE /drinks/<id>
         where <id> is the existing model id
@@ -73,25 +82,21 @@ CORS(app)
         it should require the 'delete:drinks' permission
     returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
         or appropriate status code indicating reason for failure
-'''
+"""
 
 
 # Error Handling
-'''
+"""
 Example error handling for unprocessable entity
-'''
+"""
 
 
 @app.errorhandler(422)
 def unprocessable(error):
-    return jsonify({
-        "success": False,
-        "error": 422,
-        "message": "unprocessable"
-    }), 422
+    return jsonify({"success": False, "error": 422, "message": "unprocessable"}), 422
 
 
-'''
+"""
 @TODO implement error handlers using the @app.errorhandler(error) decorator
     each error handler should return (with approprate messages):
              jsonify({
@@ -100,15 +105,15 @@ def unprocessable(error):
                     "message": "resource not found"
                     }), 404
 
-'''
+"""
 
-'''
+"""
 @TODO implement error handler for 404
     error handler should conform to general task above
-'''
+"""
 
 
-'''
+"""
 @TODO implement error handler for AuthError
     error handler should conform to general task above
-'''
+"""
